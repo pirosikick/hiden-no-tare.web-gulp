e5ea2ab846a6c2633cd10015ab42edcb60b176dc
Hiroyuki Anai <pirosikick@gmail.com>
2015-06-13 23:26:10 +0900

    もろもろ修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
 'use strict';
+var path = require('path');
+
+var isProduction = process.env.NODE_ENV === 'production';
+var outputPath = path.join(__dirname, isProduction ? '.tmp' : 'build', 'scripts');
+var plugins = [];
+
+if (isProduction) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({}))
+}
 
 module.exports = {
   context: __dirname + '/src',
@@ -6,12 +15,17 @@ module.exports = {
     client: './client'
   },
   output: {
-    path: __dirname + "/.tmp/scripts",
+    path: path.join(
+      __dirname,
+      isProduction ? '.tmp' : 'build',
+      'scripts'
+    ),
     filename: "[name].bundle.js"
   },
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader?optional=runtime' }
     ]
-  }
+  },
+  plugins: plugins
 };
